abf1d260cf83d8e38fd28fea1f2509fdbc29e37e
Craig <suesscraig112715@gmail.com>
2021-03-16 17:26:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!";
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == love 
+  else 
     return "I LOVE YOU TOO PUMPKIN!"
   else 
     return "ok"
